7ee4e6d8023794d20e13c7a77aced8ba1f802300
mohax <mohax.spb@gmail.com>
2020-08-17 20:42:02 +0300

    Use PermissionsHelper and LocationGateway in main activity to show current location.

--- app/src/main/java/ru/kuchanov/huaweiandgoogleservices/ui/MainActivity.kt
+++ app/src/main/java/ru/kuchanov/huaweiandgoogleservices/ui/MainActivity.kt
@@ -2,23 +2,48 @@ package ru.kuchanov.huaweiandgoogleservices.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 import ru.kuchanov.huaweiandgoogleservices.R
 import ru.kuchanov.huaweiandgoogleservices.analytics.Analytics
 import ru.kuchanov.huaweiandgoogleservices.analytics.EventOpenSomeScreen
+import ru.kuchanov.huaweiandgoogleservices.location.LocationGateway
+import ru.kuchanov.huaweiandgoogleservices.system.PermissionsHelper
 
 class MainActivity : AppCompatActivity() {
 
-    val analytics: Analytics by inject()
-
+    private val analytics: Analytics by inject()
+    private val permissionHelper: PermissionsHelper by inject()
+    private val locationGateway: LocationGateway by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        permissionHelper.attach(this)
+
         analytics.send(EventOpenSomeScreen())
 
         textView.setOnClickListener { analytics.send(EventOpenSomeScreen()) }
+
+        locationButton.setOnClickListener {
+            locationGateway
+                .requestLastLocation()
+                .subscribeBy(
+                    onSuccess = { Snackbar.make(root, "Location: $it", Snackbar.LENGTH_SHORT).show() },
+                    onError = {
+                        it.printStackTrace()
+                        Snackbar.make(root, "Location error: ${it.message}", Snackbar.LENGTH_SHORT).show()
+                    }
+                )
+        }
+    }
+
+    override fun onDestroy() {
+        permissionHelper.detach()
+
+        super.onDestroy()
     }
 }
