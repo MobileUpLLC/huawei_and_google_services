5c5889dace3854bcacf39374bf7ef2c8f7c3e3a6
mohax <mohax.spb@gmail.com>
2020-08-17 20:40:13 +0300

    Provide FusedLocationClient and LocationGateway by DI.

--- app/src/main/java/ru/kuchanov/huaweiandgoogleservices/di/AppModule.kt
+++ app/src/main/java/ru/kuchanov/huaweiandgoogleservices/di/AppModule.kt
@@ -3,10 +3,15 @@ package ru.kuchanov.huaweiandgoogleservices.di
 import org.koin.dsl.module
 import ru.kuchanov.huaweiandgoogleservices.analytics.Analytics
 import ru.kuchanov.huaweiandgoogleservices.analytics.AnalyticsImpl
+import ru.kuchanov.huaweiandgoogleservices.location.FusedLocationClient
+import ru.kuchanov.huaweiandgoogleservices.location.FusedLocationClientImpl
+import ru.kuchanov.huaweiandgoogleservices.location.LocationGateway
 
 object AppModule {
 
     fun create() = module {
         single<Analytics> { AnalyticsImpl(get()) }
+        single<FusedLocationClient> { FusedLocationClientImpl(get(), get()) }
+        single { LocationGateway(get()) }
     }
 }
